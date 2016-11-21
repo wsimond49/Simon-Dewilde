a9485ce9946f5458f59e76cfe5d2c5f0057c0308
Simon Dewilde <wsimond49@gmail.com>
2016-11-20 19:35:09 -0500

    Changed constructor

--- ChessGame.java
+++ ChessGame.java
@@ -25,53 +25,22 @@ public class ChessGame
         
         board = new ChessBoard();
         
-        ChessLocation initial00 = new ChessLocation(0,0);
-        Rook R1 = new Rook(player1, this, initial00);
-        
-        ChessLocation initial01 = new ChessLocation(0,1);
-        Knight N1 = new Knight(player1, this, initial01);
-        
-        ChessLocation initial02 = new ChessLocation(0,2);
-        Bishop B1 = new Bishop(player1, this, initial02);
-        
-        ChessLocation initial03 = new ChessLocation(0,3);
-        Queen Q1 = new Queen(player1, this, initial03);
-        
-        ChessLocation initial04 = new ChessLocation(0,4);
-        King K1 = new King(player1, this, initial04);
-        
-        ChessLocation initial05 = new ChessLocation(0,5);
-        Bishop B2 = new Bishop(player1, this, initial05);
-        
-        ChessLocation initial06 = new ChessLocation(0,6);
-        Knight K2 = new Knight(player1, this, initial06);
-        
-        ChessLocation initial07 = new ChessLocation(0,7);
-        Rook R2 = new Rook(player1, this, initial07);
-        
-        ChessLocation initial10 = new ChessLocation(1,0);
-        Pawn P1 = new Pawn(player1, this, initial10);
-        
-        ChessLocation initial11 = new ChessLocation(1,1);
-        Pawn P2 = new Pawn(player1, this, initial11);
-        
-        ChessLocation initial12 = new ChessLocation(1,2);
-        Pawn P3 = new Pawn(player1, this, initial12);
-        
-        ChessLocation initial13 = new ChessLocation(1,3);
-        Pawn P4 = new Pawn(player1, this, initial13);
-        
-        ChessLocation initial14 = new ChessLocation(1,4);
-        Pawn P5 = new Pawn(player1, this, initial14);
-        
-        ChessLocation initial15 = new ChessLocation(1,5);
-        Pawn P6 = new Pawn(player1, this, initial15);
-        
-        ChessLocation initial16 = new ChessLocation(1,6);
-        Pawn P7 = new Pawn(player1, this, initial16);
-        
-        ChessLocation initial17 = new ChessLocation(1,7);
-        Pawn P8 = new Pawn(player1, this, initial17);
+        Rook R1 = new Rook(player1, this, new ChessLocation(0,0));
+        Knight N1 = new Knight(player1, this, new ChessLocation(0,1));
+        Bishop B1 = new Bishop(player1, this, new ChessLocation(0,2));
+        Queen Q1 = new Queen(player1, this, new ChessLocation(0,3));
+        King K1 = new King(player1, this, new ChessLocation(0,4));
+        Bishop B2 = new Bishop(player1, this, new ChessLocation(0,5));
+        Knight K2 = new Knight(player1, this, new ChessLocation(0,6));
+        Rook R2 = new Rook(player1, this, new ChessLocation(0,7));
+        Pawn P1 = new Pawn(player1, this, new ChessLocation(1,0));
+        Pawn P2 = new Pawn(player1, this, new ChessLocation(1,1));
+        Pawn P3 = new Pawn(player1, this, new ChessLocation(1,2));
+        Pawn P4 = new Pawn(player1, this, new ChessLocation(1,3));
+        Pawn P5 = new Pawn(player1, this, new ChessLocation(1,4));
+        Pawn P6 = new Pawn(player1, this, new ChessLocation(1,5));
+        Pawn P7 = new Pawn(player1, this, new ChessLocation(1,6));
+        Pawn P8 = new Pawn(player1, this, new ChessLocation(1,7));
         
     }
 
